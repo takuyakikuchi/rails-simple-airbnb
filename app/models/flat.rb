9e3c364ecbab29ca2c37ccfe2f034e5c4e6a836d
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-11-06 16:56:48 +0900

    created index action for flats

---
+++ app/models/flat.rb
@@ -0,0 +1,7 @@
+class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night, presence: true, numericality: true
+  validates :number_of_guests, presence: true, numericality: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+puts "Creating flats..."
+
+20.times do
+  Flat.create!(
+    name: Faker::JapaneseMedia::DragonBall.character,
+    address: Faker::Nation.capital_city,
+    description: Faker::Quote.most_interesting_man_in_the_world,
+    price_per_night: (10..1000).to_a.sample,
+    number_of_guests: (1..10).to_a.sample
+  )
+end
+puts "...Finished creating flats"
